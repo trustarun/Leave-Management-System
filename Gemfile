9d84ccd2d9b47f4ce32eaf75b5c058ddb5da68ec
Arun <trustarun@yahoo.co.in>
2012-04-21 11:28:29 +0530

    rails framework created for leave management

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.2'
+gem 'json'
+gem 'nokogiri'
+gem 'execjs'
+gem 'therubyracer'
+gem 'jquery-rails'
+
+group :development do
+  gem 'rspec-rails', '2.7.0'
+  gem 'mysql', '2.8.1'
+end
+
+group :test do
+  gem 'rspec-rails', '2.7.0'
+  gem 'webrat', '0.7.1'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeaveManagementSystem::Application.config.secret_token = '2721ea85dbef1ea62c2c6461661172453e316d8cc42d2f2df639541f4f4c46e1360df00ff8f312cdd54e3e17561772e45b0a6682972b793970cc8cd39492a763'
